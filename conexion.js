7b78bd9a448cdcc838b5d85bf76f7a4e71c77c78
carlospeyrot03 <carlospeyrot0803@gmail.com>
2024-07-02 16:34:43 -0300

    conexion modelo

--- conexion.js
+++ conexion.js
@@ -1,16 +1,33 @@
-class Conexion {
-    constructor() {
-        this.conexion = new XMLHttpRequest();
-    }
+// Importar el paquete mysql
+const mysql = require('mysql');
 
-    enviarPeticion(url, metodo, datos, callback) {
-        this.conexion.open(metodo, url, true);
-        this.conexion.setRequestHeader("Content-Type", "application/json");
-        this.conexion.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                callback(this.responseText);
-            }
-        }
-        this.conexion.send(datos);
+// Configurar los parámetros de conexión
+const conexion = mysql.createConnection({
+    host: 'tu_host',
+    user: 'tu_usuario',
+    password: 'tu_contraseña',
+    database: 'tu_base_de_datos'
+});
+
+// Conectar a la base de datos
+conexion.connect((error) => {
+    if (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        return;
     }
-}
+    console.log('Conexión establecida correctamente');
+    
+    // Aquí puedes realizar consultas u otras operaciones
+});
+
+// Ejemplo de consulta
+conexion.query('SELECT * FROM tu_tabla', (error, resultados, campos) => {
+    if (error) {
+        console.error('Error al ejecutar la consulta:', error);
+        return;
+    }
+    console.log('Resultados de la consulta:', resultados);
+});
+
+// Cerrar la conexión al finalizar
+conexion.end();
