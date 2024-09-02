12367222cabd6f2f94d5d097dcfd4db928c85d7f
carlospeyrot03 <carlospeyrot0803@gmail.com>
2024-09-02 17:41:52 -0300

    a

--- frontend/inicio.js
+++ frontend/inicio.js
@@ -6,13 +6,14 @@ export class Inicio extends Phaser.Scene {
     preload() {
         this.load.image("botonInicio", "/images/botonInicio.png");
     }
-
+    
     create() {
-       
 
         let botonInicio = this.add.image(300, 350, "botonInicio").setInteractive();
+    
         botonInicio.on("pointerdown", () => {
             this.scene.start("IngresarDatos");
         });
     }
 }
+
