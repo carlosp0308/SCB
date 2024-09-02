ce1f32424ec112b5de9bd5ed1dd009dfbd72e1a2
carlospeyrot03 <carlospeyrot0803@gmail.com>
2024-09-02 19:39:26 -0300

    Agregado de imagen al fondo del inicio

--- frontend/inicio.js
+++ frontend/inicio.js
@@ -5,10 +5,14 @@ export class Inicio extends Phaser.Scene {
 
     preload() {
         this.load.image("botonInicio", "/images/botonInicio.png");
+        this.load.image("fondoInicio", "/images/fondo-inicio.png");
     }
     
     create() {
 
+        let fondoInicio = this.add.image(0, 0, "fondoInicio");
+        fondoInicio.setOrigin(0, 0);
+        fondoInicio.setDisplaySize(this.scale.width, this.scale.height);
         let botonInicio = this.add.image(300, 350, "botonInicio").setInteractive();
     
         botonInicio.on("pointerdown", () => {
